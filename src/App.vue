9e30b31a8c1c5ea9cff0beceaf40b9b49f01cb6b
captain <1610504235@qq.com>
2024-09-30 10:35:30 +0800

    v_0.1

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <SolarSystem />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SolarSystem from './components/solarsystem.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    SolarSystem,
+  },
+};
 </script>
 
 <style>
